cb2f48af89926f68835919b6bae4029c7888f691
ZhangHaojie <Loading@vip.163.com>
2021-01-13 17:11:36 +0800

    初始化openCV项目

---
+++ app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,12 @@
+#include <jni.h>
+#include <string>
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+extern "C" JNIEXPORT jstring JNICALL
+Java_com_example_opencv_MainActivity_stringFromJNI(
+        JNIEnv *env,
+        jobject /* this */) {
+    std::string hello = "Hello from C++";
+    return env->NewStringUTF(hello.c_str());
+}
